f6a5b06c428540ebbe58e66e865ee296cba7b6a6
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-07-24 19:18:27 +0300

    Add redirect to login page when token expires

--- src/OnlineTestSystem.Client/src/services/authService.js
+++ src/OnlineTestSystem.Client/src/services/authService.js
@@ -1,9 +1,13 @@
-import { setAuthorizationHeader } from "../api/api";
+import { api, setAuthorizationHeader } from "../api/api";
 import { getUserFromToken, removeToken, saveToken } from "./tokenService";
+
+let interceptor;
 
 export function authorize(token) {
     saveToken(token);
     setAuthorizationHeader(token);
+
+    setLogoutInterceptor(token);
 
     const user = getUserFromToken(token);
     return user;
@@ -12,4 +16,21 @@ export function authorize(token) {
 export function logout(){
     setAuthorizationHeader('');
     removeToken();
+    
+    if (interceptor){
+        api.interceptors.response.eject(interceptor);
+        interceptor = null;
+    }
+}
+
+function setLogoutInterceptor(token) {
+    interceptor = api.interceptors.response.use(res => res, error => {
+        if (error.response?.status !== 401 || !token) {
+            return Promise.reject(error);
+        }
+
+        if (error.response?.status === 401 || error.response?.status === 403) {
+            window.location.href = "/account/logout";
+        }
+    });
 }
