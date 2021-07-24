5ee20f896607961115ca184f243422b55f26daf3
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-07-24 18:51:33 +0300

    Add redux devtools

--- src/OnlineTestSystem.Client/src/data/store.js
+++ src/OnlineTestSystem.Client/src/data/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { testsReducer } from "./testReducer";
 import { userReducer } from "./userReducer";
@@ -8,4 +8,9 @@ const rootReducer = combineReducers({
   tests: testsReducer
 });
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
